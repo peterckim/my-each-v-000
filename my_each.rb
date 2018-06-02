2207688ec65b0ab3abe1022dab2fe194af1c314d
Peter Kim <peterckim96@gmail.com>
2018-06-02 02:15:38 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 def my_each(collection)
   count = 0
   while count < collection.length
-  puts "Before yield"
-  yield(collection)
-  puts "After yield"
+    puts "Before yield"
+    yield(collection)
+    puts "After yield"
 end
 
 collection = [1, 2, 3, 4]
