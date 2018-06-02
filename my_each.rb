ad537b5e679d52dd61949a689cf04be98aae0d9f
Peter Kim <peterckim96@gmail.com>
2018-06-02 02:05:21 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  count = 0
+  yield(collection)
+  return collection
+end
+
+collection = [1, 2, 3, 4]
+
+my_each(collection) do |i|
+  puts i
 end
